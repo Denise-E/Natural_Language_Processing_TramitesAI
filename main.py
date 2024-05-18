8298011f99d82a122f020608f50c3ebb0a7a51bf
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2024-05-18 18:48:31 -0300

    more comments

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ MODELO_ASUNTOS = ModeloAsuntosMultiClases()
 def modelo_asuntor_prediccion(sentendes: list):
     return MODELO_ASUNTOS.model_prediction_tests(sentence=sentences)
     
-    
+# Sentencias a prdecir, junto al outcome esperado
 sentences = [
-    "solicitud cotizacion póliza del hogar", 
-    "solicitud póliza del hogar", 
-    "que seas feliz", 
-    "vacaciones en Mardel", 
-    "notificación membresia", 
-    "notificación membresía"
+    "siniestro a denunciar", #1
+    "auto cotizacion", #2
+    "envio presupuestos" #4
+    "solicitud cotizacion póliza del hogar",  #3
+    "solicitud póliza del hogar",  #3
+    "que seas feliz", #0
+    "vacaciones en Mardel", #0
+    "notificación membresia", #0
+    "notificación membresía", #0
 ]
 
-print("********************* PREDICCIÓN *************************")
 print("Información modelo:" )
 print("Cantidad de iteraciones al momento de la capacitación del modelo: 3000")
 print("Cantidad datos de capacitación: 169")
 print("Cantidad datos de testeo: 42")
+print("********************* INICIO PREDICCIÓNES *************************")
 prediccion = modelo_asuntor_prediccion(sentences)
 print("SENTENCES:",sentences)
 print("PREDICTION:", prediccion)
-print("**************************************************************")
+print("********************** FIN PREDICCIÓNES **************************")
--- modelos/asuntos_multi_clases.py
+++ modelos/asuntos_multi_clases.py
@@ -6,17 +6,19 @@ from .data.data_asuntos_mc import outcomes, training_sentences as training_sente
 from keras.utils import to_categorical
 
 class ModeloAsuntosMultiClases:
-    VOCAB_SIZE = 10000 #Máx num de palabras que se deben conservar, si se le pasan más va a guardar las 10000 más repetidas.
-    EMBEDDING_DIM = 16 
-    MAX_LENGTH = 10000 
-    TRUNC_TYPE='post'
-    PADDING_TYPE='post' 
-    OOV_TOKEN = "<OOV>" #Token para todas aquellas palabras no tokenizadas.
-    tokenizer = None 
-    model = None
+    # Parámetros para la configuración del modelo
+    VOCAB_SIZE = 10000  # Máximo número de palabras que se deben conservar, si se le pasan más, guardará las 10000 más repetidas.
+    EMBEDDING_DIM = 16  # Dimensión del vector de características para cada palabra.
+    MAX_LENGTH = 10000  # Longitud máxima de las secuencias de entrada.
+    TRUNC_TYPE='post'  # Tipo de truncado de las secuencias (después de alcanzar MAX_LENGTH).
+    PADDING_TYPE='post'  # Tipo de padding (agregado de tokens al final de la secuencia).
+    OOV_TOKEN = "<OOV>"  # Token para palabras fuera del vocabulario (Out Of Vocabulary).
+    tokenizer = None  # Tokenizer se inicializa como None y se configura posteriormente.
+    model = None  # Modelo se inicializa como None y se configura posteriormente.
 
     @classmethod
     def __init__(cls):
+        # Inicializa la configuración y el entrenamiento del modelo al instanciarse la clase.
         cls.model_config_and_training()
         
     @classmethod
@@ -40,27 +42,25 @@ class ModeloAsuntosMultiClases:
         tokenizer = Tokenizer(num_words=cls.VOCAB_SIZE, oov_token=cls.OOV_TOKEN) 
         tokenizer.fit_on_texts(sentences)
         cls.tokenizer = tokenizer
-        word_indexed = tokenizer.word_index # Arma lista de las palabras tokenizadas
+        word_indexed = tokenizer.word_index # Genera la lista de las palabras tokenizadas
         print("Indexed  WORDS", word_indexed)
         
         training_sentences = tokenizer.texts_to_sequences(sentences) # Crea la secuencia de tokens para cada oración. Lista de listas.
+        
         """
         pad_sequences:
         
-        Hay que deifnirle MAX_LENGTH por concordancia con resto del proceso, sino va a romper luego.
-        Todas las sentencias deben tener la misma cantidad de palabras, eso se resuelve completando
-        con la cantidad de OOV_TOKEN necesarios en cada sentencia. 
-        Se completará con el OOV_TOKEN al final, dado por el valor 'post' del parámetro padding.
-        Por ejemplo, en un caso de una sentencia formada por 3 palarbas que deba llevarse a una longitud de 
-        5 palabras quedaría luego del este método formado de la siguiente manera: [1,2,3,<OOV>,<OOV>]
+        - MAX_LENGTH debe definirse por concordancia con el resto del proceso.
+        - Todas las oraciones deben tener la misma cantidad de palabras, se resuelve completando con la cantidad de OOV_TOKEN necesarios en cada oración.
+        - Se completará con el OOV_TOKEN al final, dado por el valor 'post' del parámetro padding.
+        Por ejemplo, en una oración de 3 palabras que deba llevarse a una longitud de 5 palabras, quedará [1,2,3,<OOV>,<OOV>]
         """
         training_padded = pad_sequences(training_sentences,maxlen=cls.MAX_LENGTH, padding='post')  
         training_labels = to_categorical(training_labels, num_classes=outcomes_quantity)
         print("PADDED", training_padded)
         #print("PADDED SHAPE", training_padded.shape) #Filas x columns - muestra cantidades
         
-        # 3. Creación del Modelo
-        # TODO Documentar
+        # 3. Creación y configuración del modelo de red neuronal
         model = tf.keras.Sequential([
             tf.keras.layers.Embedding(cls.VOCAB_SIZE, cls.EMBEDDING_DIM, input_length=cls.MAX_LENGTH),
             tf.keras.layers.GlobalAveragePooling1D(),
@@ -90,7 +90,7 @@ class ModeloAsuntosMultiClases:
 
         num_epochs = 3000 # Cantidad de iteraciones que hará el modelo durante el entrenameinto
         print("TEST RESULTS")
-        #Se le pasan inputs y outputs de los datos de capacitación y de testeo. Acá es entrenado el motor.
+        # Entrenamiento del modelo con los datos de entrenamiento y de prueba.
         history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
         """
         Resultados model.fit:
@@ -106,6 +106,6 @@ class ModeloAsuntosMultiClases:
         padded = pad_sequences(sequences, maxlen=cls.MAX_LENGTH, padding=cls.PADDING_TYPE, truncating=cls.TRUNC_TYPE)
         # Con la sentencia recibida ya tokenizada, le pedimos al modelo que haga la predicción.
         prediction = cls.model.predict(padded)
-        return [prediction.tolist() , np.argmax(prediction, axis=1)] #Valor a ajustar una vez que vaya sumando casos para la capacitación del modelo
-        #return np.argmax(prediction, axis=1)
+        return [prediction.tolist() , np.argmax(prediction, axis=1)]
+        #return np.argmax(prediction, axis=1) # Devuelve la clase predicha con mayor probabilidad.
     
