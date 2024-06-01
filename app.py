bb7c3a38bb9255539e82b8d7b1e91b33d50a43e0
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2024-06-01 20:34:25 -0300

    important comment on app.py

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask_cors import cross_origin
 
 app = Flask(__name__)
 
+"""
+Hasta la implementación de las rutas, para correr el modelo de asuntos se deberá correr el archivo main_asuntos.py,
+el cuál se encuentra dentro de la carpeta modelos > asuntos.
+
+Para correr el modelo de pólizas de autos, se debe correr el archivo tramites_ner.py, el cuál se encuentra dentro de la
+carpeta modelos > tramites
+"""
+
+
 # Routes
 @app.route("/ping", methods=['GET']) 
 @cross_origin()
