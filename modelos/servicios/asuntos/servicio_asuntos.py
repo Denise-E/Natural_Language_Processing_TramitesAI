c3fbc4dbe7c41051131d2df6449e0ca7a0adc375
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2024-06-20 12:40:37 -0300

    comments

--- modelos/servicios/asuntos/servicio_asuntos.py
+++ modelos/servicios/asuntos/servicio_asuntos.py
@@ -4,7 +4,7 @@ import logging
 import shutil
 import os
 
-# Suprimir advertencias
+# Suprimir logs de tensorflow
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
 MODELO_RUTA = os.getenv("MODELO_ASUNTO_GUARDADO")
@@ -23,14 +23,9 @@ class ServicioAsuntos(ServicioModelos):
         num_epochs = data.get("num_epochs") if 'num_epochs' in data else 4000
             
         cls.modelo_asuntos = ModeloTensorFlow(vocab_size=vocab_size, embedding=embedding, max_length=max_length, num_epochs=num_epochs, model_path=MODELO_RUTA)
-
         
     @classmethod
     def predecir(cls, sentencias: list) -> list:
         predicciones = cls.modelo_asuntos.predecir(sentencias)
         return predicciones
     
-    '''@classmethod
-    def __init__(cls, vocab_size: int = 10000, embedding: int = 16, max_length: int = 10000, num_epochs: int = 4000):
-        super().__init__(vocab_size, embedding, max_length, num_epochs, MODELO_RUTA)
-        print("Servicio de asuntos inicializado")'''
